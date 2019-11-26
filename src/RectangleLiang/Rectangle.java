52a50b11ec9aa21a3c26521048345a7ebb59e7c2
Nicki Hvolgaard <38239769+TheMagicalKing@users.noreply.github.com>
2019-11-26 13:33:06 +0100

    Caught some exceptions in the TryCatch package and edited Rectangle to actually have a constructor.

--- src/RectangleLiang/Rectangle.java
+++ src/RectangleLiang/Rectangle.java
@@ -3,34 +3,34 @@ package RectangleLiang;
 public class Rectangle {
 
     //------- Variables
-    double width = 1;
-    double height = 1;
+    private double width = 1;
+    private double height = 1;
     //------- Constructors
-    private void RectangleLiang(){}
-    private void RectangleLiang(double width, double height){
+    protected Rectangle(){}
+    protected Rectangle(double width, double height){
     }
     //------- Methods
     public void outputArea(double inputHeight, double inputWidth){
         double width=inputWidth;
         double height=inputHeight;
-        getArea(width,height);
+        System.out.println(calcArea(width,height));
 
     }
     public void outputPeri(double inputHeight, double inputWidth){
         double width=inputWidth;
         double height=inputHeight;
-        getPerimeter(width,height);
-
+        calcPerimeter(width,height);
+        System.out.println(calcPerimeter(width,height));
     }
 
-    private void getArea(double width,double height){
+    private double calcArea(double width,double height){
         double area = width*height;
-        System.out.println(area);
+        return area;
     }
 
-    private void getPerimeter(double width,double height){
+    private double calcPerimeter(double width,double height){
         double perimeter = width+width+height+height;
-        System.out.println(perimeter);
+        return perimeter;
     }
 
 }
--- src/TryCatch/TryCatchTesting.java
+++ src/TryCatch/TryCatchTesting.java
@@ -1,4 +1,24 @@
 package TryCatch;
 
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+import java.util.Scanner;
+
 public class TryCatchTesting {
+
+    public static void main(String[] args) {
+        while (true){
+            System.out.println("Skriv et tal");
+            Scanner s = new Scanner(System.in);
+            int tal = s.nextInt();
+
+            try{
+                System.out.println(100/tal);
+            }catch (ArithmeticException e){
+                System.out.println("Du må ikke dividere med nul, prøv igen! Fejlkode: " + e );
+            }
+            if (tal== 300){break;}
+
+        }
+    }
+
 }
